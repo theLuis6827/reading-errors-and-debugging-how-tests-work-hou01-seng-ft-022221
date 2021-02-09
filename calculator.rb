3c16acfddcf6d44bf8cc1642eb9d13d673a88911
Luis E Lozano <lozano_22_lalo@yahoo.com>
2021-02-09 00:17:13 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,4 @@
-  first_number = num1
-  second_number = num2
-  sum = first_number + second_number
-  return sum
+first_number = num1
+second_number = num2
+sum = first_number + second_number
+return sum
