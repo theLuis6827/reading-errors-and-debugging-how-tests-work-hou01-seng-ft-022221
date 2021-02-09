9b585530e4de540564a8b9dcd0b0737b4cdc8e82
Luis E Lozano <lozano_22_lalo@yahoo.com>
2021-02-09 00:14:31 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 def addition (first_number, second_number, sum)
-  first_number = Integer
-  second_number = Integer
+  first_number = num1
+  second_number = num2
   sum = first_number + second_number
   return sum
 end
